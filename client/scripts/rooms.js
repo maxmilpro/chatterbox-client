bf949dc6fb8dc068cfb32f30100ccaafb9dfe216
Max Proano <proanom89@gmail.com>
2020-11-20 12:41:18 -0800

    Initialize rooms

--- client/scripts/rooms.js
+++ client/scripts/rooms.js
@@ -1,4 +1,11 @@
 var Rooms = {
 
+  fetch: function() {
+    Parse.readAll((data) => {
+      // call MessageView.render on the data
+      console.log('chatterbox: Rooms retrieved'),
+      RoomsView.render(data);
+    });
+  }
 
 };
--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -4,9 +4,27 @@ var RoomsView = {
   $select: $('#rooms select'),
 
   initialize: function() {
+    //access the messages in the database
+    Rooms.fetch();
   },
 
-  render: function() {
+  render: function(data) {
+    // create a containerArr = rooms
+    var roomList = [];
+    // iterate over the data
+    for (room of data.results) {
+      // if the current room is not already in the rooms array
+      if (room.roomname && !_.contains(roomList, room.roomname)) {
+        // add the room to the rooms array
+        roomList.push(room.roomname);
+      }
+    }
+    // iterate over the rooms array
+    for (room of roomList) {
+      var compiled = _.template('<option> <%- roomName %>  </option>');
+      // add each to the room select
+      RoomsView.$select.append(compiled({'roomName': room}));
+    }
   }
 
 };
